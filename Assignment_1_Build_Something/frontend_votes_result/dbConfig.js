894d6bdfeb83211b8a7c4a1f28b81c11c020e4ba
msaeedghaderi <m.saeedghaderi@gmail.com>
2025-09-26 20:17:48 +0200

    Update_2025-09-26_20:17

---
+++ Assignment_1_Build_Something/frontend_votes_result/dbConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'db',
+    user: 'root',
+    password: 'password',
+    database: 'votesDB',
+    waitForConnections: true, 
+    connectionLimit: 10,
+    queueLimit: 0
+};
